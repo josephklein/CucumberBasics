534991f2a7cf8aeef1f388ad0a455830678cd3bb
Joe Klein <jklein@covermymeds.com>
2018-02-19 15:21:44 -0500

    joes cucumber solution

--- features/animal.feature
+++ features/animal.feature
@@ -1,1 +1,18 @@
-#Create your feature here
+Feature: Animal
+  In order to properly identify guests
+  As a hotel manager
+  I want to store their names, types, ages, and if they are old
+
+  Scenario: Tom
+    Given a "cat" named "Tom" that is 5 years old
+    Then its type should be "cat"
+    And its name should be "Tom"
+    And its age should be 5
+    And it is old
+
+  Scenario: Jerry
+    Given a "mouse" named "Jerry" that is 2 years old
+    Then its type should be "mouse"
+    And its name should be "Jerry"
+    And its age should be 2
+    And it is not old
--- features/step_definitions/animal_steps.rb
+++ features/step_definitions/animal_steps.rb
@@ -1,1 +1,23 @@
-#Delete this comment, here is where you should write your step defs
+Given(/^a "(.*?)" named "(.*?)" that is (\d+) years old$/) do |type, name, age|
+  @animal = Animal.new name, type, age.to_i
+end
+
+Then(/^its type should be "(.*?)"$/) do |type|
+  expect(@animal.type).to eq type
+end
+
+Then(/^its name should be "(.*?)"$/) do |name|
+  expect(@animal.name).to eq name
+end
+
+Then(/^its age should be (\d+)$/) do |age|
+  expect(@animal.age).to eq age.to_i
+end
+
+Then(/^it is not old$/) do
+  expect(@animal.old?).to eq false
+end
+
+Then(/^it is old$/) do
+  expect(@animal.old?).to eq true
+end
